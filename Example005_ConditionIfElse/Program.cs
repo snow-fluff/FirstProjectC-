838ac57b513bc3f02f28899ae6d8344fbbfad12d
Романенко Александр Григорьевич <Romanenko_AG@app-sol.ru>
2022-11-27 21:49:51 +0300

    Add Example 005 Condition If / Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string UserName = Console.ReadLine();
+if(UserName.ToLower() == "саша")
+{  
+    Console.WriteLine("Ура, это же Саша!");
+}
+else
+{
+        Console.Write("Привет, ");
+        Console.WriteLine(UserName);
+}
